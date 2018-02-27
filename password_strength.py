61af627ee8ac22a26d777dbf1ead02733cda1466
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-02-27 13:56:28 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -3,7 +3,9 @@ import sys
 
 def count_upper_letters(password):
     amount_of_upper_letters = 0
-    upper_letters = set(chr(index) for index in range(65, 91)) | set(chr(index) for index in range(1040, 1072)) | {'Ё'}
+    eng_upper_letters = set(chr(index) for index in range(65, 91))
+    rus_upper_letters = set(chr(index) for index in range(1040, 1072)) | {'Ё'}
+    upper_letters = eng_upper_letters | rus_upper_letters
     for letter in password:
         if letter in upper_letters:
             amount_of_upper_letters += 1
@@ -12,7 +14,9 @@ def count_upper_letters(password):
 
 def count_lower_letters(password):
     amount_of_lower_letters = 0
-    lower_letters = set(chr(index) for index in range(97, 123)) | set(chr(index) for index in range(1072, 1104)) | {'ё'}
+    eng_lower_letters = set(chr(index) for index in range(97, 123))
+    rus_lower_letters = set(chr(index) for index in range(1072, 1104)) | {'ё'}
+    lower_letters = eng_lower_letters | rus_lower_letters
     for letter in password:
         if letter in lower_letters:
             amount_of_lower_letters += 1
@@ -30,7 +34,9 @@ def count_numbers(password):
 
 def count_symbols(password):
     amount_of_symbols = 0
-    symbols = set(chr(index) for index in range(33, 48)) | set(chr(index) for index in range(58, 65))
+    first_group_of_symbols = set(chr(index) for index in range(33, 48))
+    second_group_of_symbols = set(chr(index) for index in range(58, 65))
+    symbols = first_group_of_symbols | second_group_of_symbols
     for letter in password:
         if letter in symbols:
             amount_of_symbols += 1
@@ -40,14 +46,15 @@ def count_symbols(password):
 def count_repeats_of_letters(password):
     sum_of_repeats = 0
     max_repeat = 0
-    repeat_now = 0
     number_of_letter = 0
     while number_of_letter < len(password) - 1:
         if password[number_of_letter] == password[number_of_letter + 1]:
             repeat_now = 1
-            while number_of_letter < len(password) - 1 and password[number_of_letter] == password[number_of_letter + 1]:
+            while password[number_of_letter] == password[number_of_letter + 1]:
                 repeat_now += 1
                 number_of_letter += 1
+                if number_of_letter == len(password) - 1:
+                    break
             sum_of_repeats += repeat_now
             if max_repeat < repeat_now:
                 max_repeat = repeat_now
@@ -58,26 +65,27 @@ def count_repeats_of_letters(password):
 def additions(password):
     number_of_characters = len(password) * 4
     upper_letters = (len(password) - count_upper_letters(password)) * 2
-    lower_letter =  (len(password) - count_lower_letters(password)) * 2
+    lower_letters = (len(password) - count_lower_letters(password)) * 2
     numbers = count_numbers(password) * 4
     symbols = count_symbols(password) * 6
-    requirements = (len(password) >= 8 and upper_letters > 0 and lower_letter > 0 and numbers > 0 and symbols > 0) * 10
-    return number_of_characters + upper_letters + numbers + symbols + requirements
+    type_criteria = upper_letters + lower_letters + numbers + symbols
+    all_types = upper_letters * lower_letters * numbers * symbols > 0
+    requirements = (number_of_characters >= 8 and all_types) * 10
+    return number_of_characters + type_criteria + requirements
 
 
 def deductions(password):
-    numbers_only = (password.isdigit()) * len(password) * 10
-    upper_only = (password.isupper() and password.isalpha()) * len(password) * 6
-    lower_only = (password.islower() and password.isalpha()) * len(password) * 6
-    letters_only = (password.isalpha()) * len(password) * 2
-    only_criteria = numbers_only + upper_only + lower_only + letters_only
+    numbers_only = (password.isdigit())
+    upper_only = (password.isupper() and password.isalpha())
+    lower_only = (password.islower() and password.isalpha())
+    only_criteria = numbers_only * 10 + (upper_only + lower_only) * 7
     not_numbers = (not count_numbers(password)) * 9
     not_upper = (not count_upper_letters(password)) * 7
     not_lower = (not count_lower_letters(password)) * 7
     not_symbols = (not count_symbols(password)) * 5
     not_criteria = not_numbers + not_upper + not_lower + not_symbols
     repeats_of_letters = count_repeats_of_letters(password)
-    return only_criteria + not_criteria + repeats_of_letters
+    return only_criteria * len(password) + not_criteria + repeats_of_letters
 
 
 def get_password_strength(password):
